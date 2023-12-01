55e7f05b7c33e9936c88d53c7aa6069a9d47e3cf
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-12-01 20:46:08 -0300

    Cebolinha repete

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string fraseDigitada, fraseCebolinha;
+
+Console.Write("Digite uma frasease: ");
+fraseCebolinha = Console.ReadLine()!;
+
+fraseComDislalia = fraseCebolinha
+    .Replace("r", "l")
+    .Replace("R", "L");
+
+Console.WriteLine($"Cebolinha: {fraseCebolinha}");
